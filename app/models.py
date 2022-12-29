f6371e4fe57845aa636b6a059cd0e3007387eb90
Abe Winter <awinter.public@gmail.com>
2022-12-29 18:49:36 -0500

    as_uuid to fix crash w/ uuids, schemaversion.latest

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ from sqlalchemy.dialects.postgresql import JSONB, UUID, BYTEA
 
 class Base(Model):
     __abstract__ = True
-    id: uuid.UUID = Column(UUID, primary_key=True, default=uuid.uuid4)
+    id: uuid.UUID = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     created: datetime = Column(DateTime, default=func.now())
 
 class TaskSchema(Base):
@@ -20,7 +20,7 @@ class TaskSchema(Base):
 class ApiKey(Base):
     "a key which external providers use to call in"
     key: bytes = Column(BYTEA, primary_key=True)
-    tschema_id: Optional[uuid.UUID] = Column(UUID, ForeignKey('task_schema.id'), nullable=True) # recommended to restrict this to a specific provider, but not required
+    tschema_id: Optional[uuid.UUID] = Column(UUID(as_uuid=True), ForeignKey('task_schema.id'), nullable=True) # recommended to restrict this to a specific provider, but not required
     tschema = relationship('TaskSchema')
     permissions: List[str] = Column(JSONB)
     active: bool = Column(Boolean, default=True)
@@ -51,9 +51,19 @@ class SchemaVersion(Base):
         UniqueConstraint('tschema_id', 'semver'),
     )
 
+    @classmethod
+    def latest(cls, query: 'Optional[sqlalchemy.sql.Select]' = None, schema_name: Optional[str] = None) -> 'sqlalchemy.sql.Select':
+        "extend a query to use the latest schema version. optionally also specify the schema name"
+        # todo: use inner select instead of join().limit(1) so it doesn't force the cardinality of the other tables to be 1
+        query = query or sqlalchemy.select(cls)
+        query = query.join(cls).order_by(sqlalchemy.text('version desc')).limit(1)
+        if schema_name:
+            query = query.join(TaskSchema).where(TaskSchema.name == schema_name)
+        return query
+
 class TaskType(Base):
     "within a SchemaVersion, a named task"
-    version_id: uuid.UUID = Column(UUID, ForeignKey('schema_version.id'))
+    version_id: uuid.UUID = Column(UUID(as_uuid=True), ForeignKey('schema_version.id'))
     version = relationship('SchemaVersion')
     name: str = Column(String)
     pending_states: List[str] = Column(JSONB)
@@ -68,7 +78,7 @@ class TaskType(Base):
 
 class Task(Base):
     "an instance of a TaskType"
-    ttype_id: uuid.UUID = Column(UUID, ForeignKey('task_type.id'))
+    ttype_id: uuid.UUID = Column(UUID(as_uuid=True), ForeignKey('task_type.id'))
     ttype = relationship('TaskType')
     state: Optional[str] = Column(String, nullable=True)
     user_id: Optional[int] = Column(Integer, ForeignKey('ab_user.id'), nullable=True) # assigned user
@@ -76,7 +86,7 @@ class Task(Base):
 
 class TaskHistory(Base):
     "task changelog"
-    task_id: uuid.UUID = Column(UUID, ForeignKey('task.id'))
+    task_id: uuid.UUID = Column(UUID(as_uuid=True), ForeignKey('task.id'))
     task = relationship('Task')
     state: Optional[str] = Column(String, nullable=True)
     resolved: bool = Column(Boolean, default=False)
